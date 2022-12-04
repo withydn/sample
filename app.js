1f207f5e0525d984e192f82af40bed52854ab9da
withydn <withydn@naver.com>
2022-12-04 16:51:06 +0900

    ok

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ app.set('view engine', 'ejs');
 app.use('/', indexRouter);
 app.use('/users', userRouter);
 
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// app.get('/users', (req, res) => {
+//   const users = await db.getUsers();
+//   res.render('users', { users });
+// });
+
 app.listen(PORT, () => {
   console.log(`${PORT} 포트에서 서버 실행 중`);
 });
